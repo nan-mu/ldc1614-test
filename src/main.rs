fad394cde999ef046776a26d9c5b1d5547ed5607
nan mu <mu.nan.11@outlook.com>
2024-10-21 11:06:29 +0000

    docs(参数计算方式): 添加4个函数的设置说明
    from_inductance_capacitance; to_clock_dividers; (Fsensor已完成) set_clock_dividers;
    set_conv_settling_time;

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 // i2c_ds3231.rs - Sets and retrieves the time on a Maxim Integrated DS3231
 // RTC using I2C.
 
@@ -6,8 +5,8 @@ use std::error::Error;
 use std::thread;
 use std::time::Duration;
 
-use rppal::i2c::{self, I2c};
 use ldc1x1x as ldc;
+use rppal::i2c::{self, I2c};
 
 //ADDR接地时ldc1614的地址
 const ADDR_LDC1614: u16 = 0x2A;
@@ -22,13 +21,12 @@ fn dec2bcd(dec: u8) -> u8 {
 }
 
 fn main() {
-
-    let mut  i2c=I2c::new()?;
+    let mut i2c = I2c::new()?;
 
     i2c.set_slave_address(ADDR_LDC1614)?;
 
     //创建实例，初始化LDC设备
-    let mut ldc = ldc::Ldc::new(i2c,ADDR_LDC1614);
+    let mut ldc = ldc::Ldc::new(i2c, ADDR_LDC1614);
 
     ldc.set_sleep_mode(true).unwrap();
 
@@ -36,15 +34,12 @@ fn main() {
 
     //计算分频，这里需要理论计算
     let div = ldc::Fsensor::from_inductance_capacitance(12.583, 100.0).to_clock_dividers(None);
-    
-     //配置传感器
+
+    //配置传感器
     for ch in [ldc::Channel::Zero] {
-
-
-
         //为指定通道设置时钟分频器，这一参数控制采样速度，影响数据读取的频率。
         //ldc.set_clock_dividers(ch, div).unwrap();
-        ldc.set_clock_dividers(ch,div).unwrap();
+        ldc.set_clock_dividers(ch, div).unwrap();
 
         //设置转换稳定时间（settling time）为 40 个时钟周期。等待一段时间以确保信号稳定。
         ldc.set_conv_settling_time(ch, 40).unwrap();
@@ -61,8 +56,7 @@ fn main() {
     ldc.set_mux_config(
         ldc::MuxConfig::default()
             .with_auto_scan(false)
-            .with_deglitch_filter_bandwidth(ldc::Deglitch::ThreePointThreeMHz)
-
+            .with_deglitch_filter_bandwidth(ldc::Deglitch::ThreePointThreeMHz),
     )
     .unwrap();
     ldc.set_config(ldc::Config::default()).unwrap();
@@ -76,9 +70,6 @@ fn main() {
     // timing ignored because polling with a cp2112 with no delays is slow enough already
     // outputting just newline separated numbers so you can feed it into https://github.com/mogenson/ploot
     loop {
-        println!(
-            "{}",
-            ldc.read_data_24bit(ldc::Channel::Zero).unwrap(),
-        );
+        println!("{}", ldc.read_data_24bit(ldc::Channel::Zero).unwrap(),);
     }
 }

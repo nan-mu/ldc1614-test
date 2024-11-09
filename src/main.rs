6ead8bfe03b9e1866aed399e5bf4db2e1607094c
nan mu <mu.nan.11@outlook.com>
2024-11-09 10:04:31 +0000

    perf(motor): 修改电机控制结构

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use rppal::gpio::Gpio;
+use rppal::gpio::{self, Gpio, OutputPin};
 use tokio::sync::TryAcquireError;
 
 #[derive(Parser, Debug)]
@@ -17,48 +17,57 @@ struct Args {
     #[clap(short, long, default_value_t = 0)]
     channel: u8,
 }
-/// GPIO模拟PWM输出
-/// 首先说明电机的连接方式是共阴极连接，
-/// ENA-、DIR-、PUL-接控制器的地，
-/// ENA+接使能信号，DIR+接方向信号物理口32，PUL+接脉冲信号物理口40
-async fn generate_pwm_signal(
-    frequency: f64,
-    duty_cycle: f64,
-    microstepping: u8,
-    travel_distance: f64, // 滑轨前进的距离（毫米）
-) {
-    use tokio::time::Duration;
 
-    let mut pwm = Gpio::new().unwrap().get(21).unwrap().into_output();
+struct Motor {
+    pwm: OutputPin,
+    dir: OutputPin,
+}
 
-    // 启动 PWM 输出
-    pwm.set_pwm_frequency(frequency, duty_cycle).unwrap();
+#[derive(Debug)]
+enum Error {
+    Gpio(gpio::Error),
+}
 
-    // 计算速度 (mm/s)，滑轨每转的距离是1.0mm，步进电机在没有细分的情况下，电机每步进一次时的角度为1.8°，
-    let speed = frequency * 1.0 * 1.8 / (microstepping as f64 * 360.0);
-
-    //设置方向
-    let mut dir = Gpio::new().unwrap().get(12).unwrap().into_output();
-    let mut travel_distance2 = travel_distance;
-    if travel_distance.is_sign_negative() {
-        dir.set_low();
-        travel_distance2 = -travel_distance;
-    } else {
-        dir.set_high();
+impl From<gpio::Error> for Error {
+    fn from(value: gpio::Error) -> Self {
+        Error::Gpio(value)
     }
+}
 
-    // 计算滑轨前进指定距离需要的时间 (秒)
-    let travel_time = travel_distance2 / speed;
+type Result<T> = core::result::Result<T, Error>;
 
-    // 异步等待滑轨前进指定距离
-    tokio::time::sleep(Duration::from_secs_f64(travel_time)).await;
-
-    // 停止 PWM 输出
-    pwm.clear_pwm().unwrap();
+impl Motor {
+    /// GPIO模拟PWM输出
+    /// 首先说明电机的连接方式是共阴极连接，
+    /// ENA-、DIR-、PUL-接控制器的地，
+    /// ENA+接使能信号，DIR+接方向信号物理口32，PUL+接脉冲信号物理口40
+    /// * distance 电机运动距离（正数为正向移动）
+    async fn moving(&mut self, distance: f64) -> Result<()> {
+        use tokio::time::{self, Duration};
+        // 电机运动速度 (mm/s)= 脉冲频率 * 丝杆导程 * 电机旋转步长 / (编码器细分度 * 360°)
+        // > 滑轨每转的距离是1.0mm，步进电机在没有细分的情况下，电机每步进一次时的角度为1.8°
+        const SPEED: f64 = 1.5625;
+        // 启动 PWM 输出
+        self.pwm.set_pwm_frequency(5000.0, 0.5)?;
+        //设置方向引脚
+        match distance.is_sign_negative() {
+            true => {
+                self.dir.set_low();
+            }
+            false => {
+                self.dir.set_high();
+            }
+        }
+        // 异步等待滑轨前进指定距离
+        time::sleep(Duration::from_secs_f64(distance.abs() / SPEED)).await;
+        // 停止 PWM 输出
+        self.pwm.clear_pwm()?;
+        Ok(())
+    }
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     // 解析命令行参数
     let args = Args::parse();
     assert!(args.channel <= 3, "错误：通道只能选择0, 1, 2, 3");
@@ -92,6 +101,12 @@ async fn main() {
     let ldc = Ldc::<0x2b>::new(&mut i2c);
     ldc.defaule_config(&mut i2c, real_channel).unwrap();
 
+    debug!("初始化gpio");
+    let gpio = Gpio::new().unwrap();
+    let pwm = gpio.get(21).unwrap().into_output_low();
+    let dir = gpio.get(12).unwrap().into_output_high();
+    let mut motor = Motor { pwm, dir };
+
     // debug!("连接数据库");
     // let client = redis::Client::open("redis://:mypassword@127.0.0.1/").unwrap();
     // let mut connect = client.get_multiplexed_tokio_connection().await.unwrap();
@@ -115,7 +130,7 @@ async fn main() {
 
     // 调用生成 PWM 信号的函数
     //频率为1000hz，占空比50%，细分度16，让滑轨前进10mm
-    generate_pwm_signal(5000.0, 0.5, 16, 10.0).await;
+    motor.moving(10.0).await.unwrap();
 
     /*
     loop {
@@ -136,4 +151,5 @@ async fn main() {
         // )
     }
     */
+    Ok(())
 }
